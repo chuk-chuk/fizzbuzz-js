6503e346112facea6aba9e42565a6b3faf280bd8
James Ransome <jamesransome@msn.com>
2017-08-01 12:54:52 +0100

    Buzz if divisible by 5

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -3,4 +3,9 @@ describe('FizzBuzz', function() {
   it('should return fizz if numbers divisible by 3', function(){
     expect(FizzBuzz(3)).toEqual("Fizz");
   });
+
+  it("should return 'Buzz' if number is divisible by 5", function(){
+    expect(FizzBuzz(5)).toEqual("Buzz");
+  });
+
 });
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,3 +1,9 @@
-function FizzBuzz(){
-  return 'Fizz';
+function FizzBuzz(number){
+  if (number % 3 == 0){
+    return 'Fizz';
+  }
+  else if (number % 5 == 0) {
+    return 'Buzz';
+  }
+
 };
