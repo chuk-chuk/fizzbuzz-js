90636d53549e9116e51ab89b1c2515e2140cb320
chuk-chuk <yulia.v.litvin@gmail.com>
2017-08-01 13:37:52 +0100

    refactored and add mathod play

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,19 +1,29 @@
 describe('FizzBuzz', function() {
+  var fizzBuzz;
 
-  it('should return fizz if numbers divisible by 3', function(){
-    expect(FizzBuzz(3)).toEqual("Fizz");
+  beforeEach(function(){
+    fizzBuzz = new FizzBuzz();
   });
+
+  describe('multiples of 3', function(){
+    it('should return fizz if numbers divisible by 3', function(){
+      expect(fizzBuzz.play(3)).toEqual("Fizz");
+    });
+  });
+
 
   it("should return 'Buzz' if number is divisible by 5", function(){
-    expect(FizzBuzz(5)).toEqual("Buzz");
+    expect(fizzBuzz.play(5)).toEqual("Buzz");
   });
 
+
+
   it("should return 'FizzBuzz' if number divisible by 3 & 5", function(){
-    expect(FizzBuzz(15)).toEqual("FizzBuzz");
+    expect(fizzBuzz.play(15)).toEqual("FizzBuzz");
   });
 
   it("should return the number if not divisible by 3 or 5", function(){
-    expect(FizzBuzz(38)).toEqual(38);
+    expect(fizzBuzz.play(38)).toEqual(38);
   });
 
 });
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,16 +1,22 @@
-function FizzBuzz(number){
-  if (number % 15 == 0) {
+function FizzBuzz() {}
+
+FizzBuzz.prototype.play = function(number) {
+  if (this.isDivisibleBy(15, number)) {
     return 'FizzBuzz';
-  }
-
-  else if (number % 3 == 0){
+  } else if (this.isDivisibleBy(3, number)) {
     return 'Fizz';
-  }
-  else if (number % 5 == 0) {
+  } else if (this.isDivisibleBy(5, number)) {
     return 'Buzz';
-  }
-
-  else {
+  } else {
     return number;
   }
 };
+
+FizzBuzz.prototype.isDivisibleBy = function(divisor, number) {
+  return number % divisor === 0;
+}
+
+var fizzBuzz = new FizzBuzz();
+for (var i = 1; i <= 100; i++) {
+  console.log(fizzBuzz.play(i));
+}
